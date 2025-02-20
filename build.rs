c513324af79d949fcd93adec7b9ff7d86e33f1df
Mohammed Alyousef <may642_2000@hotmail.com>
2025-02-21 01:10:47 +0300

    use tailwind for styling

---
+++ build.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("cargo:rerun-if-changed=templates");
+
+    let dir: String = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let input = format!("{dir}/templates/input.css");
+    let output = format!("{dir}/assets/main.css");
+
+    let result = std::process::Command::new("npx")
+        .args(["@tailwindcss/cli", "-i", &input, "-o", &output])
+        .output()
+        .expect("Unable to generate css");
+
+    if !result.status.success() {
+        let error = String::from_utf8_lossy(&result.stderr);
+        println!("cargo:warning=Unable to build CSS !");
+        println!("cargo:warning=Output: {error}");
+    }
+}
